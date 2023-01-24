ff071ec3a193934213a8d3559d778643617e6f19
Mateusz <mat.bereda@gmail.com>
2023-01-24 15:56:26 +0100

    nowy commit

--- src/main/java/pl/camp/micro/git2/Main.java
+++ src/main/java/pl/camp/micro/git2/Main.java
@@ -3,5 +3,7 @@ package pl.camp.micro.git2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("kod !!");
+
+        System.out.println("zmiany !!");
     }
 }
